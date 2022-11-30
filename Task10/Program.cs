0c1c3b25d1cdadb83426ec3dc3a41c799ab855cc
Malibu <malibu090@mail.ru>
2022-11-30 19:52:55 +0300

    Homework2 is ready

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = (numberA % 100) / 10;
+Console.WriteLine($"вторая цифра -> " + numberB);
---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+Console.WriteLine("Введите число ");
+int numberA = int.Parse(Console.ReadLine());
+if (99 < numberA & numberA < 1000)
+{
+    numberA = numberA % 10;
+    Console.WriteLine($"третья цифра -> {numberA}");
+}
+else if (numberA < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (numberA > 999)
+    {
+        numberA = numberA / 10;
+    }
+    numberA = numberA % 10;
+    Console.WriteLine($"третья цифра -> {numberA}");
+}
